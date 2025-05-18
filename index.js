0b6b491cd71cd6085511a02a7a643e3bc2d56237
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-05-18 11:59:06 +0545

    cors

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ const dbConnect = require("./Database/db");
 dbConnect();
 
 // middleware
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://hosms-mern.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'auth-token']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 
 
 // apis
 app.use("/api/auth",require("./routes/Auth"));
-app.use("/api/doctors",require("./routes/AppointmentRoute"));
 app.use("/api/appointment",require("./routes/AppointmentRoute"));
 app.use("/api/contact",require("./routes/Contact_route"));
 
